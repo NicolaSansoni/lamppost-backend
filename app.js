21b99844759fb437b003b6c0fee1b86fedd25496
Nicola Sansoni <n.sansoni97@gmail.com>
2020-12-18 11:53:44 +0100

    Refactor: remove ';'

--- app.js
+++ app.js
@@ -1,38 +1,38 @@
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const express = require('express')
+const cookieParser = require('cookie-parser')
+const logger = require('morgan')
 
 /* Configuration of the server that communicates with the TCU */
-const indexRouter = require('./routes');
+const indexRouter = require('./routes')
 
-const app = express();
+const app = express()
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
 
-app.use('/', indexRouter);
+app.use('/', indexRouter)
 
 /* Configuration of the server that communicates with the AI module */
-const internalRouter = require('./routes/internal');
+const internalRouter = require('./routes/internal')
 
-const appInternal = express();
+const appInternal = express()
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
 
-app.use('/', internalRouter);
+app.use('/', internalRouter)
 
 /* Jobs */
-const jobs = {};
+const jobs = {}
 
-const {sendDataToServer} = require('./engine/lamppost');
+const {sendDataToServer} = require('./engine/lamppost')
 
-const timerUpdateStatus = 5 * 60 * 1000;
+const timerUpdateStatus = 5 * 60 * 1000
 
-jobs.updateStatus = setInterval(sendDataToServer, timerUpdateStatus);
+jobs.updateStatus = setInterval(sendDataToServer, timerUpdateStatus)
 
-module.exports = {app, appInternal, jobs};
+module.exports = {app, appInternal, jobs}
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.send('lamppost backend');
-});
+  res.send('lamppost backend')
+})
 
-module.exports = router;
+module.exports = router
